4aa8af4e0fb39d66024f4cbca50999b389e43cf6
papikelscode <nwakanmakelechi8@gmail.com>
2022-05-31 16:50:43 +0100

    responsiveness fixed

---
+++ assets/js/index.js
@@ -0,0 +1,157 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 8000); // Change image every 2 seconds
+}
+/* user defined variables */
+var timeOnSlide = 3, 		
+    // the time each image will remain static on the screen, measured in seconds
+timeBetweenSlides = 1, 	
+    // the time taken to transition between images, measured in seconds
+
+// test if the browser supports animation, and if it needs a vendor prefix to do so
+    animationstring = 'animation',
+    animation = false,
+    keyframeprefix = '',
+    domPrefixes = 'Webkit Moz O Khtml'.split(' '), 
+    // array of possible vendor prefixes
+    pfx  = '',
+    slidy = document.getElementById("slidy"); 
+if (slidy.style.animationName !== undefined) { animation = true; } 
+// browser supports keyframe animation w/o prefixes
+
+if( animation === false ) {
+  for( var i = 0; i < domPrefixes.length; i++ ) {
+    if( slidy.style[ domPrefixes[i] + 'AnimationName' ] !== undefined ) {
+      pfx = domPrefixes[ i ];
+      animationstring = pfx + 'Animation';
+      keyframeprefix = '-' + pfx.toLowerCase() + '-';
+      animation = true;
+      break;
+    }
+  }
+}
+
+if( animation === false ) {
+  // animate in JavaScript fallback
+} else {
+  var images = slidy.getElementsByTagName("img"),
+      firstImg = images[0], 
+      // get the first image inside the "slidy" element.
+      imgWrap = firstImg.cloneNode(false);  // copy it.
+  slidy.appendChild(imgWrap); // add the clone to the end of the images
+  var imgCount = images.length, // count the number of images in the slide, including the new cloned element
+      totalTime = (timeOnSlide + timeBetweenSlides) * (imgCount - 1), // calculate the total length of the animation by multiplying the number of _actual_ images by the amount of time for both static display of each image and motion between them
+      slideRatio = (timeOnSlide / totalTime)*100, // determine the percentage of time an induvidual image is held static during the animation
+      moveRatio = (timeBetweenSlides / totalTime)*100, // determine the percentage of time for an individual movement
+      basePercentage = 100/imgCount, // work out how wide each image should be in the slidy, as a percentage.
+      position = 0, // set the initial position of the slidy element
+      css = document.createElement("style"); // start marking a new style sheet
+  css.type = "text/css";
+  css.innerHTML += "#slidy { text-align: left; margin: 0; font-size: 0; position: relative; width: " + (imgCount * 100) + "%;  }\n"; // set the width for the slidy container
+  css.innerHTML += "#slidy img { float: left; width: " + basePercentage + "%; }\n";
+  css.innerHTML += "@"+keyframeprefix+"keyframes slidy {\n"; 
+  for (i=0;i<(imgCount-1); i++) { // 
+    position+= slideRatio; // make the keyframe the position of the image
+    css.innerHTML += position+"% { left: -"+(i * 100)+"%; }\n";
+    position += moveRatio; // make the postion for the _next_ slide
+    css.innerHTML += position+"% { left: -"+((i+1) * 100)+"%; }\n";
+}
+  css.innerHTML += "}\n";
+  css.innerHTML += "#slidy { left: 0%; "+keyframeprefix+"transform: translate3d(0,0,0); "+keyframeprefix+"animation: "+totalTime+"s slidy infinite; }\n"; // call on the completed keyframe animation sequence
+document.body.appendChild(css); // add the new stylesheet to the end of the document
+}
+
+jQuery(document).ready(function ($) {
+	var feedbackSlider = $(".feedback-slider");
+	feedbackSlider.owlCarousel({
+		items: 1,
+		nav: true,
+		dots: true,
+		autoplay: true,
+		loop: true,
+		mouseDrag: true,
+		touchDrag: true,
+		navText: [
+			"<i class='fa fa-long-arrow-left'></i>",
+			"<i class='fa fa-long-arrow-right'></i>"
+		],
+		responsive: {
+			// breakpoint from 767 up
+			767: {
+				nav: true,
+				dots: false
+			}
+		}
+	});
+
+	feedbackSlider.on("translate.owl.carousel", function () {
+		$(".feedback-slider-item h3")
+			.removeClass("animated fadeIn")
+			.css("opacity", "0");
+		$(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating")
+			.removeClass("animated zoomIn")
+			.css("opacity", "0");
+	});
+
+	feedbackSlider.on("translated.owl.carousel", function () {
+		$(".feedback-slider-item h3").addClass("animated fadeIn").css("opacity", "1");
+		$(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating")
+			.addClass("animated zoomIn")
+			.css("opacity", "1");
+	});
+	feedbackSlider.on("changed.owl.carousel", function (property) {
+		var current = property.item.index;
+		var prevThumb = $(property.target)
+			.find(".owl-item")
+			.eq(current)
+			.prev()
+			.find("img")
+			.attr("src");
+		var nextThumb = $(property.target)
+			.find(".owl-item")
+			.eq(current)
+			.next()
+			.find("img")
+			.attr("src");
+		var prevRating = $(property.target)
+			.find(".owl-item")
+			.eq(current)
+			.prev()
+			.find("span")
+			.attr("data-rating");
+		var nextRating = $(property.target)
+			.find(".owl-item")
+			.eq(current)
+			.next()
+			.find("span")
+			.attr("data-rating");
+		$(".thumb-prev").find("img").attr("src", prevThumb);
+		$(".thumb-next").find("img").attr("src", nextThumb);
+		$(".thumb-prev")
+			.find("span")
+			.next()
+			.html(prevRating + '<i class="fa fa-star"></i>');
+		$(".thumb-next")
+			.find("span")
+			.next()
+			.html(nextRating + '<i class="fa fa-star"></i>');
+	});
+	$(".thumb-next").on("click", function () {
+		feedbackSlider.trigger("next.owl.carousel", [300]);
+		return false;
+	});
+	$(".thumb-prev").on("click", function () {
+		feedbackSlider.trigger("prev.owl.carousel", [300]);
+		return false;
+	});
+}); //end ready
